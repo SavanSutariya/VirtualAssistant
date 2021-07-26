f2755b48f1e3cec668fb10ed7235c4e82d00b08b
savan <savanpatel5248@gmail.com>
2021-07-26 19:10:40 +0530

    check platform before running

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import os
+import platform
 from input_module import take_input
 from process_module import process
 from output_module import output
 from greeting_module import greet
 
-os.system("cls")
+# clear screen before starting
+if(platform.uname().system == 'Windows'):
+    # Windows
+    os.system("cls")
+elif(platform.uname().system == 'Darwin' or platform.uname().system == 'linux'):
+    # Mac and linux
+    os.system("clear")
 greet()
 while (True):
     i = take_input()
--- system_details.py
+++ system_details.py
@@ -1,2 +1,2 @@
 from database import get_name
-sys_name = get_name("assistant name")
+sys_name = get_name("assistant name") #check asssis name from database
