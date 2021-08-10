3ef3800ccc4a70a9ae22ef7b27734ef14aa2b8a6
Savan Sutariya <savanpatel5248@gmail.com>
2021-08-10 18:49:02 +0530

    simple changes

--- internet.py
+++ internet.py
@@ -4,6 +4,7 @@ import wikipedia
 import webbrowser
 from database import get_web_dir,add_web_dir
 from gui.input_module import gui_input
+from output_module import output
 def internet_access():
     try:
         urllib.request.urlopen('http://google.com')
@@ -26,14 +27,16 @@ def open_web(query):
     site = query.replace("open ","")
     webdir = dict(get_web_dir(site))
     if(bool(webdir)):
+        output('Opening '+site)
         return webbrowser.open_new_tab(webdir[site])
     else:
         return False
 def add_new_web_address():
+    output('Enter site name')
     site_name = gui_input('Site Name')
+    output('Enter site address')
     site_address = gui_input('Site Address')
     if(site_name.replace(' ','') == '' and site_address.replace(' ','') == ''):
         return "Both the fields are required"
     else:
         return add_web_dir(site_name,site_address)
-
