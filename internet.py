8bc42a43fece3f511f3236c4c51015cc8aa88183
Savan Sutariya <savanpatel5248@gmail.com>
2021-09-18 13:09:02 +0530

    Wikipedia bug fix

--- internet.py
+++ internet.py
@@ -12,17 +12,21 @@ def internet_access():
     except:
         return False
 def check_wikipedia(query):
-    query = query.lower()
-    query = query.replace("who is","")
-    query = query.replace("what is","")
-    query = query.replace("do you know","")
-    query = query.replace("tell me about","")
-    query = query.strip()
-    try:
-        data = wikipedia.summary(query, sentences=2)
-        return "wikipedia says, "+data
-    except Exception as e:    
-        return ""
+    if internet_access():
+        query = query.lower()
+        query = query.replace("who is","")
+        query = query.replace("what is","")
+        query = query.replace("do you know","")
+        query = query.replace("tell me about","")
+        query = query.strip()
+        try:
+            data = wikipedia.summary(query, sentences=2)
+            return "wikipedia says, "+data
+        except Exception as e:    
+            return None
+    else:
+        return "No connection"
+        
 def open_web(query):
     site = query.replace("open ","")
     webdir = dict(get_web_dir(site))
