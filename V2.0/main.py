ed9bf53c0c8a315ff012318d88e5134a6b099d45
Savan Sutariya <savanpatel5248@gmail.com>
2023-05-18 20:08:21 +0530

    Basic speking and listening

--- V2.0/main.py
+++ V2.0/main.py
@@ -1,3 +1,21 @@
+import win32com.client
+import speech_recognition as sr
+def speak(text):
+    speaker = win32com.client.Dispatch("SAPI.SpVoice")
+    speaker.Speak(text)
+
+def listen():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio, language="en-in")
+            print(text)
+        except:
+            print("Sorry, I did not get that")
+    return text
 if __name__ == '__main__':
-    print("Update")
+    # speak("hello")
+    speak(listen())
     
